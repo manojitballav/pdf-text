6f471df233f90e2a4a3d86d8d1a83d8730f87b80
manojitballav <manojitocto@gmail.com>
2020-03-02 23:44:30 +0530

    Added script which converts pdf and stores all data into a text file

---
+++ index.py
@@ -0,0 +1,14 @@
+import pdftotext
+# https://github.com/jalan/pdftotext
+
+# Load your PDF
+with open("test.pdf", "rb") as f:
+    pdf = pdftotext.PDF(f)
+# get the number of pages
+print(len(pdf))
+
+# creating a text file
+file = open("test.txt", "w")
+for page in pdf:
+    file.write(page)
+file.close()
